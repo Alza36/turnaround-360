064566ebd94556bbd8678f7f042454204ee15d04
Alza36 <joseosorioribeirosilva@gmail.com>
2026-04-14 04:11:47 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Turnaround 360 - Sistema Online!');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
